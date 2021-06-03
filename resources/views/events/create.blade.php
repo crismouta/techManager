5524cfad857e8854978cd7b582f0a0306772c3e5
Jael Masllorens <jmasllorens@gmail.com>
2021-06-03 16:41:01 +0200

    Cambio mayúsculas create

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Create a new event') }}
+            {{ __('Create a New Event') }}
         </h2>
     </x-slot>
 
