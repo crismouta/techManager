8f8dd53d070b8ab0eecd9915a9a5526a0d77c6be
Jael Masllorens <jmasllorens@gmail.com>
2021-06-03 16:50:47 +0200

    último cambio

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            <em>{{ __('Lore')}}</em><em>meets</em>&nbsp;&nbsp;{{('Tech Events') }}
+            <em>{{ __('Lore')}}meets</em>&nbsp;&nbsp;{{('Tech Events') }}
         </h2>
     </x-slot>
     <!-- component -->
