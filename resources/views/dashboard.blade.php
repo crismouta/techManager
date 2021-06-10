bf3f8acf8198afbcfd1c1d42b06b3b65b2b1e49b
Cris Mouta <crismouta@gmail.com>
2021-06-10 17:18:37 +0200

    edit show admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -346,7 +346,6 @@
                 </div>
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">Laravel 8 for Beginners</div>
                 <div class="text-sm text-gray-500">{{$event->description}}</div>
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap">
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -5,7 +5,71 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <!-- component -->
+<div class="flex justify-between m-6">
+  <div class="flex flex-col h-full max-w-lg mx-auto bg-green-100 rounded-lg">
+          <img
+            class="rounded-lg rounded-b-none"
+            src="{{ asset('storage').'/'.$event->image}}"
+            alt="thumbnail"
+            loading="lazy"
+          />
+          <div class="flex justify-between -mt-4 px-4">
+            <span
+              class="inline-block ring-4 bg-green-600 ring-gray-800 text-sm font-medium tracking-wide text-gray-100 px-3 pt-0.5"
+              >TED Talk</span
+            >
+            <span
+              class="flex h-min space-x-1 items-center rounded-full text-gray-400 bg-gray-800 py-1 px-2 text-xs font-medium"
+            >
+              
+              <p class="text-blue-500 font-semibold text-xs">
+                5 / {{$event->capacity}}
+              </p>
+            </span>
+          </div>
+          <div class="py-2 px-4">
+            <h1
+              class="text-xl font-medium leading-6 tracking-wide text-black hover:text-blue-500 cursor-pointer">
+              {{$event->title}}
+            </h1>
+          </div>
+          <div class="px-4 space-y-2">
+            <p class="text-gray-600 font-normal leading-5 tracking-wide">
+              {{$event->description}}
+            </p>
+          </div>
+          <div class="flex flex-row items-end h-full w-full px-4 mt-4">
+            <div class="flex border-t border-gray-700 w-full py-4">
+              <div
+                class="flex items-center space-x-3 border-r border-gray-700 w-full"
+              >
+                <img
+                  class="object-cover w-8 h-8 border-2 border-white rounded-sm"
+                  src="https://scontent-mad1-1.xx.fbcdn.net/v/t1.6435-9/48367594_2254870701505746_1892060332294144000_n.png?_nc_cat=110&ccb=1-3&_nc_sid=973b4a&_nc_ohc=RTQYj1zVyXUAX9P_14R&_nc_ht=scontent-mad1-1.xx&oh=911d3d8bc253e459c5460962a04e66b3&oe=60E8458A"
+                  alt="profile users"
+                  loading="lazy"
+                />
+                <div class="">
+                  <p class="text-sm font-semibold tracking-wide text-black">
+                    Author
+                  </p>
+                  <p class="text-xs font-light tracking-wider text-gray-600">
+                    2 Hours ago
+                  </p>
+                </div>
+              </div>
+              <div class="flex items-center flex-shrink-0 px-2">
+                <div class="flex items-center space-x-1 text-gray-400">
+                  <a href="{{url('/events/edit/'.$event->id)}}" class="bg-white text-indigo-600 hover:text-indigo-900">Edit</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+</div>
+
+    {{-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
@@ -47,7 +111,6 @@
                 </div>
               </td>
               <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">Laravel 8 for Beginners</div>
                 <div class="text-sm text-gray-500">Life is CRUD(e)</div>
               </td>
               <td class="px-6 py-4 whitespace-nowrap">
@@ -57,20 +120,22 @@
               </td>
               <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                 28/09/2021 - 19/10/2021
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+              </td> --}}
+
+              <!--Edit-->
+              {{-- <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
              
                 <a href="{{url('/events/edit/'.$event->id)}}" class="bg-white text-indigo-600 hover:text-indigo-900">Edit</a>
             
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+              </td> --}}
+              {{-- <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                <form action="{{ url('/events/'.$event->id)}}" method="post">
                 @csrf
                 {{method_field('DELETE')}}
                   <input type="submit" class="bg-white text-red-600 hover:text-red-900" 
                   onclick="return confirm('Are you sure you want to permanently remove this item?')" value="Delete">
                 </form>
-              </td>
+              </td> --}}
               
             </tr>
             
