193118e5bb62e19b6f476c567571ca9b81de15ed
Fresh! <francocalvino89@gmail.com>
2021-06-10 11:27:21 +0200

    creacion de eventos base

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,11 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create(['name' => 'root', 'email' => 'root@root.com']);
         User::factory(10)->create();
-        Event::factory(30)->create();
+        /* Event::factory()->create(['title' => 'Women in technology', 'description' => 'A case for positive            discrimination', 'image'=>'', 'capacity'=>'']); */
+        Event::factory()->create(['title' => 'Agile mindset nobody expects the Spanish inquisition', 'description' => 'How to become friends with adversity', 'image'=>'img/agile-mindset.jpg', 'capacity'=>'17']);
+        /* Event::factory()->create(['title' => 'Break into Web Development', 'description' => 'How to get started as a web developer and build solid apps', 'image'=>'', 'capacity'=>'']);
+        Event::factory()->create(['title' => 'Populism: a defense', 'description' => 'Why all politicians should be populists', 'image'=>'', 'capacity'=>'']);
+        Event::factory()->create(['title' => 'Laravel 8 for Begginers', 'description' => 'Life is CRUD(e)', 'image'=>'', 'capacity'=>'']);
+ */
     }
 }
