ccf4243dd54521d284bdf6f081930546e9c17ea9
blaucomfuig <scorcuera6@gmail.com>
2021-06-17 14:03:22 +0200

    se ha habilitado button radio para indicar que un event oes favorito o no, de cara al slider

--- database/migrations/2021_06_02_101254_create_events_table.php
+++ database/migrations/2021_06_02_101254_create_events_table.php
@@ -16,7 +16,7 @@ class CreateEventsTable extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->dateTime('date');
-            $table->boolean('isFavorite')->default(false);
+            $table->boolean('isFavorite')->default(0);
             $table->string('title');
             $table->string('description')->nullable();
             $table->string('image')->nullable();
